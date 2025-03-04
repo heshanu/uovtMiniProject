abc72a604b8adc23151da95bde161eb1f73a3f9f
heshanu <heshanu97@gmail.com>
2025-03-04 18:47:25 +0530

    not completed calender part

--- src/app/compoent/customerdetail/customerdetail.component.ts
+++ src/app/compoent/customerdetail/customerdetail.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CalenderService } from '../../service/calender.service';
 
 @Component({
   selector: 'app-customerdetail',
@@ -10,7 +11,11 @@ export class CustomerdetailComponent {
   
   registrationForm!:FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder,private calenderService:CalenderService) {}
+
+  photo: File | null = null;
+  photoError: string | null = null;
+  days!:any;
 
   ngOnInit(): void {
     this.registrationForm = this.fb.group({
@@ -18,7 +23,13 @@ export class CustomerdetailComponent {
       age: ['', [Validators.required]],
       address: ['', [Validators.required]],
       travelMode:['',Validators.required],
+      foodList:['',Validators.required],
+      foodListOption:[],
+      beverageList:['',Validators.required],
+      beverageListOption:[],
     });
+
+    this.days=this.calenderService.getDayClasses;
   }
 
 
@@ -26,9 +37,25 @@ export class CustomerdetailComponent {
     console.log(this.registrationForm.value);
     if (this.registrationForm.valid) {
       console.log('Form Submitted:', this.registrationForm.value);
+      console.log(this.days);
+      
       // Add your registration logic here (e.g., call a service)
     } else {
       console.log('Form is invalid');
     }
   }
+
+  onFileChange(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      if (file.type.startsWith('image/')) {
+        this.photo = file;
+        this.photoError = null;
+      } else {
+        this.photo = null;
+        this.photoError = 'Please upload a valid image file.';
+      }
+    }
+  }
+
 }
