404c4d5a42ec737554f177957394536f21d8dfef
heshanu <heshanu97@gmail.com>
2025-03-25 05:04:33 +0530

    not fixed

--- src/app/module/hotel/hotellist/hotellist.component.ts
+++ src/app/module/hotel/hotellist/hotellist.component.ts
@@ -1,11 +1,4 @@
 import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
-import { MaterialModule } from '../../../shared/materialModule';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatExpansionModule, MatAccordion } from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-hotellist',
@@ -15,7 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 })
 export class HotellistComponent {
 
-  items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+  items = ['Mode of travel', 'Hotel accomadation', 'Food cusine', 'Beverage', 'Item 5'];
   expandedIndex = 0;
 
 }
