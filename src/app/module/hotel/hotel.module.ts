0dc3e08514994b4c12300fdd2a6ef991255cd05a
heshanu <heshanu97@gmail.com>
2025-03-25 22:10:49 +0530

    fixed

--- src/app/module/hotel/hotel.module.ts
+++ src/app/module/hotel/hotel.module.ts
@@ -4,6 +4,7 @@ import { HotellistComponent } from './hotellist/hotellist.component';
 import { HotelRoutingModule } from './hotel-routing.module';
 import { MaterialModule } from '../../shared/materialModule';
 import { TravelModeComponent } from './travel-mode/travel-mode.component';
+import { CustomerObjectService } from '../../service/customer-object.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +15,9 @@ import { TravelModeComponent } from './travel-mode/travel-mode.component';
     CommonModule,
     HotelRoutingModule,
     MaterialModule,
-  
     ],
   exports:[
-  ]
+  ],
+  providers:[CustomerObjectService]
 })
 export class HotelModule { }
--- src/app/module/hotel/travel-mode/travel-mode.component.ts
+++ src/app/module/hotel/travel-mode/travel-mode.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-travel-mode',
   templateUrl: './travel-mode.component.html',
   styleUrl: './travel-mode.component.css'
 })
-export class TravelModeComponent {
+export class TravelModeComponent implements OnInit{
+constructor(private router:Router,private activatedRoute:ActivatedRoute){}
+  ngOnInit(): void {
+ 
+    throw new Error('Method not implemented.');
+  }
+
 
 }
