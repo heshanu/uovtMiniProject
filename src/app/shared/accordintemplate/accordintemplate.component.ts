366f3274eae8f603b9148941b13a468120509f83
heshanu <heshanu97@gmail.com>
2025-03-29 15:11:58 +0530

    couldnt fix

--- src/app/shared/accordintemplate/accordintemplate.component.ts
+++ src/app/shared/accordintemplate/accordintemplate.component.ts
@@ -1,9 +1,11 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { HotelserviceService } from '../../service/hotelservice.service';
 import { HotelsListInterface } from '../../model/hotel_interface';
-import { Observable, take } from 'rxjs';
+import { Observable, Subscription, take } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import { selectCustomerId } from '../../store/customers/customer.selectors';
 
 @Component({
   selector: 'app-accordintemplate',
@@ -11,44 +13,38 @@ import { Store } from '@ngrx/store';
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './accordintemplate.component.css'
 })
-export class AccordintemplateComponent implements OnInit{
+export class AccordintemplateComponent implements OnInit,OnDestroy{
 
 hotelsList:HotelsListInterface[]=[];
 
-customerId$!: Observable<string>;
-
+customerId$!: Observable<string|undefined>;
+customerId:string|undefined;
   id:any;
+customerIdSubscription!:Subscription;
 
   constructor(private router:Router,private activatedRoute: ActivatedRoute,
-    private store: Store<{ customerId: string }>
+    private store: Store<AppState>
   ){
-    this.customerId$ = this.store.select('customerId');
+    this.customerId$ = this.store.select(selectCustomerId);
+  }
+
+  ngOnDestroy(): void {
+    this.customerIdSubscription.unsubscribe();
   }
  
-  
   itemRecived:any[]=[];
 
       ngOnInit(): void {
-        this.itemRecived=this.items;
-       // this.hotelsList=this.hotelsListService.
+      this.customerIdSubscription=this.customerId$.subscribe((data)=>{
+        this.id=data;
+      })
       }
       
     @Input() items:any[] = [];
     
 
   navigateTo(link: string) {
-    console.log(link);
-    // Subscribe to the customerId$ observable
-    this.customerId$.pipe(
-      take(1) // Take only the current value and auto-unsubscribe
-    ).subscribe(customerId => {
-      if (!customerId) {
-        console.error('No customer ID available');
-        return;
-      }
-      this.id=customerId
-
-  
+    console.log("accordintemplate",this.id);
       this.router.navigate(["customerDashboard/",this.id,"hotelslist",link])
         .then((nav: boolean) => {
           console.log('Navigation successful:', nav);
@@ -56,7 +52,6 @@ customerId$!: Observable<string>;
         .catch((err: Error) => {
           console.error('Navigation error:', err);
         });
-    });
   }
     
    
