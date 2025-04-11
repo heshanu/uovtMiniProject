bc2d95cb5c30e9eedaab1186898ad3733184212b
heshanu <heshanu97@gmail.com>
2025-04-12 02:05:59 +0530

    fixed

--- src/app/shared/beverageconfirm/beverageconfirm.component.ts
+++ src/app/shared/beverageconfirm/beverageconfirm.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { ConfirmationModalComponent } from '../confirmation-modal/confirmation-modal.component';
 
 @Component({
   selector: 'app-beverageconfirm',
@@ -11,44 +12,23 @@ export class BeverageconfirmComponent {
    readonly dialog = inject(MatDialog);
  
   packages:any[]=[
-        {name:"Basic" ,value:" LKR 500/day"},
-        {name:"Standard",value:"LKR 1500/day"},
-        {name:"Premium ",value:"LKR 2000/day"},
-        {name:"Luxury ",value: "LKR 4000/day"},
-        {name:"Weekly Deal",value:" LKR 2000/week"},
-        {name:"Monthly Special",value:"LKR 15000/month"}
+        {name:"small" ,value:" LKR 500"},
+        {name:"Standard",value:"LKR 1500"},
+        {name:"medium ",value:"LKR 2000"},
+        {name:"Large ",value: "LKR 4000"},
+        {name:"GrandPa",value:" LKR 2000"},
+        {name:"Monthly Special",value:"LKR 15000"}
     ]
-
-    // {hotelId:"hotel1",hotelName:"Chaaya trans hotel",address:"hikkaduwa",avaliableRooms:30,img:'https://www.bestoflanka.com/images/slider/stay-in-the-beach-sri-lanka/hotel-chaaya-tranz/01.jpg',numOfStars:4,
-    //   package:[
-    //     "Basic - LKR 1500/day",
-    //     "Standard - LKR 2500/day",
-    //     "Premium - LKR 4000/day",
-    //     "Luxury - LKR 6000/day",
-    //     "Weekly Deal - LKR 12000/week",
-    //     "Monthly Special - LKR 35000/month"
-    //   ]},
  
    constructor(
      public dialogRef: MatDialogRef<BeverageconfirmComponent>,
      @Inject(MAT_DIALOG_DATA) public data: any
    ) {}
- 
-   getPackageName(pkg: string): string {
-     return pkg.split(' - ')[0];
-   }
- 
-   getPackagePrice(pkg: string): string {
-     return pkg.split(' - ')[1].split('/')[0];
-   }
- 
-   getPackageDuration(pkg: string): string {
-     return pkg.split(' - ')[1].split('/')[1];
-   }
+
  
    bookPackage(pkg:any,enterAnimationDuration: string, exitAnimationDuration: string) {
     console.log(pkg);
-    this.dialog.open(BeverageconfirmComponent, {
+    this.dialog.open(ConfirmationModalComponent, {
      width: '250px',
      enterAnimationDuration,
      exitAnimationDuration,
