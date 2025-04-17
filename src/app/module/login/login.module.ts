2dff4eec1c4af655bafc7b75092f95584f70294e
heshanu <heshanu97@gmail.com>
2025-04-17 22:59:53 +0530

    fixed

--- src/app/module/login/login.module.ts
+++ src/app/module/login/login.module.ts
@@ -5,24 +5,18 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule, FormsModule, FormGroup } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { MaterialModule } from '../../shared/materialModule';
 
 
-@NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    CommonModule,
-    LoginRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
-    HttpClientModule,
-    MaterialModule 
-  ]
-})
+@NgModule({ declarations: [
+        LoginComponent
+    ], imports: [BrowserModule,
+        CommonModule,
+        LoginRoutingModule,
+        ReactiveFormsModule,
+        FormsModule,
+        MaterialModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class LoginModule {
 
  }
